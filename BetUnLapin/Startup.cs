3cd478c216ddbd1fa26c9fd5ab12a016fa052725
Paul <cyberprime99@gmail.com>
2019-02-28 17:56:17 +0100

    Added view controllers bootstrap and others

--- BetUnLapin/Startup.cs
+++ BetUnLapin/Startup.cs
@@ -5,16 +5,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BetUnLapin
 {
     public class Startup
     {
+
+        public IConfiguration Configuration { get; set; }
+
+        
+        public Startup(IHostingEnvironment environment)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json",false,true)
+                .AddJsonFile($"appsetings{environment.EnvironmentName}.json",true,true);
+            this.Configuration = builder.Build();
+        }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +39,14 @@ namespace BetUnLapin
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Bet un Lapin!");
             });
+
+            
         }
     }
 }
